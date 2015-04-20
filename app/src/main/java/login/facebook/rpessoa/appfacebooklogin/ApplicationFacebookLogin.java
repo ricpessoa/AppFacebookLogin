89b509c1e065996178f68bddd76299ff29448422
rpessoa <linuxrp@hotmail.com>
2015-04-20 20:28:33 +0100

    Getting start Android sdk - install and import

---
+++ app/src/main/java/login/facebook/rpessoa/appfacebooklogin/ApplicationFacebookLogin.java
@@ -0,0 +1,47 @@
+package login.facebook.rpessoa.appfacebooklogin;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by rpessoa on 20/04/15.
+ */
+public class ApplicationFacebookLogin extends Application{
+    private static final String TAG = ApplicationFacebookLogin.class.getCanonicalName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG,"Application started");
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        getToken();
+
+    }
+
+    private void getToken(){
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "login.facebook.rpessoa.appfacebooklogin",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
